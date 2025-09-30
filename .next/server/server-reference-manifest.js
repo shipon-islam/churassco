2773a16123b6cb564721a3fab642178c1366c343
shipon islam <shiponkhan566@gmail.com>
2025-09-30 17:33:36 +0600

    added port number in .env

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WCTPfLyMrVYsT7oodcSuT8fLiM5RANKlxwKOVJx9ff8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6bCmY59n/TJxC17G+RLgpyt1VGm3DZmQMgfm4DMbeRU=\"\n}"
